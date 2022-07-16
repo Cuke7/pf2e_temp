9f7f6205e20e2d4f579624b8314987522fe4724a
Louis Cassany <30923342+Cuke7@users.noreply.github.com>
2022-07-16 18:28:48 +0200

    Add files via upload

--- test.js
+++ test.js
@@ -3,11 +3,14 @@ let db = require('./index.js')
 
 // db.getDataSets("classes").then(res => console.log(res))
 
-// console.log(db.translate("Breath Weapon"))
+//console.log(db.translateToFr("Breath Weapon"))
 
 // db.getItemById("feCnVrPPlKhl701x").then(res => console.log(res))
 
 
-db.getItemById("GUnw9YXaW3YyaCAU", "en").then(function (res) {
-    console.log(res)
-})
+//db.getItemById("GUnw9YXaW3YyaCAU", "en").then(function (res) {
+//    console.log(res)
+//})
+
+//db.getDataEn("GUnw9YXaW3YyaCAU").then(res => console.log(res))
+db.getDataFr("GUnw9YXaW3YyaCAU").then(res => console.log(res))
